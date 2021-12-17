95ce107217c900218f50dce89e99cd1b6607b3bd
v_weidulin <v_weidulin@tencent.com>
2021-12-17 16:15:36 +0800

    * 4 添加excel 模版使用

--- 1模版.py
+++ 1模版.py
@@ -1,2 +1,16 @@
-print('Hello world')
 # git 同步
+# excel 生成函数
+import pandas as pd
+import numpy as np
+
+
+def createExcel():
+    data = np.arange(1, 101).reshape((10, 10))
+    print(data)
+    data_df = pd.DataFrame(data)
+    data_df.columns = ['11', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J']
+    data_df.index = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']
+
+    writer = pd.ExcelWriter('my.xlsx')
+    data_df.to_excel(writer, float_format='%.5f')
+    writer.save()
