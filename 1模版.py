17d4071fb1d1311946ce4385953b3b0086984a10
v_weidulin <v_weidulin@tencent.com>
2022-04-25 11:31:11 +0800

    * 修改全局变量

--- 1模版.py
+++ 1模版.py
@@ -1,6 +1,6 @@
 # git 同步
 # excel 生成函数
-import pandas as pd
+import pandas as pd3
 import numpy as np
 from fake_user_agent.main import user_agent
 
@@ -8,11 +8,11 @@ from fake_user_agent.main import user_agent
 def createExcel():
     data = np.arange(1, 101).reshape((10, 10))
     print(data)
-    data_df = pd.DataFrame(data)
+    data_df = pd3.DataFrame(data)
     data_df.columns = ['11', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J']
     data_df.index = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']
 
-    writer = pd.ExcelWriter('my.xlsx')
+    writer = pd3.ExcelWriter('my.xlsx')
     data_df.to_excel(writer, float_format='%.5f')
     writer.save()
 
